f9edf16be15cd8a151c09687a13447fb8c0e4d54
cbeery <git@curtbeery.com>
2025-07-28 17:13:14 -0500

    Updates config for active job, solid queue

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Cbweb3
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #### CB mods
+    config.active_job.queue_adapter = :solid_queue
   end
 end
